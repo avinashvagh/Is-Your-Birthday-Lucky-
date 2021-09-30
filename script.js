b7209bde86a5e6c0e9dd601bd02dfe4a1705ceef
Avinash Vagh <78526868+avinashvagh@users.noreply.github.com>
2021-09-30 20:04:50 +0530

    fixed: clicking button

--- script.js
+++ script.js
@@ -1,43 +1,33 @@
-const dateOfBirth = document.querySelector
-("#date-of-birth");
-const luckyNumber = document.querySelector
-("#lucky-number");
-const checkNumberButton = document.querySelector
-("#check-number");
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
 const outputBox = document.querySelector("#output-box");
 
-function compareValues(sum, luckyNumber)
-
-{
-  if (sum%luckyNumber===0){
-    console.log("Your birthday is lucky ");
-
-  }else{
-    console.log("your birthday is not lucky ");
-    
+function compareValues(sum, luckyNumber) {
+  if (sum % luckyNumber === 0) {
+    outputBox.innerText = "Your birthday is lucky ";
+  } else {
+    outputBox.innerText = "your birthday is not lucky ";
   }
 }
 
-function checkBirthDateIsLucky(){
-  const dob =dateOfBirth.value;
-  const sum = calculateSum(dob);
-  if (sum&dob)
-  compareValues(sum, luckyNumber.value)
-  else
-  outputBox.innetText ="Please enter both the fields";
+function checkBirthDateIsLucky() {
+  const dob = dateOfBirth.value;
+  const sum = calculationSum(dob);
+  if (luckyNumber.value != "" && dob != "") {
+    compareValues(sum, luckyNumber.value);
+  } else {
+    outputBox.innerText = "Please enter both the fields";
+  }
 }
 
-function calculationSum(date){
-  dob = dob.replaceAll("-","");
-  let sum=0;
-  for (let index=0; index<dob.length; index++){
-    sum = sum + dob.charAt(index);
-
+function calculationSum(date) {
+  date = date.replaceAll("-", "");
+  let sum = 0;
+  for (let index = 0; index < date.length; index++) {
+    sum = sum + date.charAt(index);
   }
   return sum;
 }
 
-
-checkNumberButton.addEventListener('click', function getValues(){
-  console.log (dateOfBirth.value, luckyNumber.value);
-})
+checkNumberButton.addEventListener("click", checkBirthDateIsLucky);
